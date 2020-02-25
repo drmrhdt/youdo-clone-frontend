4c8e2846d6fb3ba3c790099f615d94632b94ac31
drmrhdt <drmrhdt@gmail.com>
2020-02-25 13:43:40 +0300

    Add functionality: add query-example to input onclick

--- youdo-clone/src/app/jumbotron/jumbotron.component.ts
+++ youdo-clone/src/app/jumbotron/jumbotron.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-jumbotron',
-  templateUrl: './jumbotron.component.html',
-  styleUrls: ['./jumbotron.component.scss']
+  selector: "app-jumbotron",
+  templateUrl: "./jumbotron.component.html",
+  styleUrls: ["./jumbotron.component.scss"]
 })
 export class JumbotronComponent implements OnInit {
+  exampleQuery: string = "подключить водонагреватель";
+  inputValue: string = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  moveTextToInput(): void {
+    this.inputValue = this.exampleQuery;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
