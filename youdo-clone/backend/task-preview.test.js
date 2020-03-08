e49e81b68e8d4872a7245725afa8442fc56b3193
drmrhdt <drmrhdt@gmail.com>
2020-03-08 19:05:38 +0300

    Fix TaskPreview model, add new info to task-preview test data

--- youdo-clone/backend/task-preview.test.js
+++ youdo-clone/backend/task-preview.test.js
@@ -4,15 +4,28 @@ module.exports = [
     author: "Асхат Ы.",
     category: {
       title_en: "Category",
-      title_ru: "Маникюр и педикюр"
+      title_ru: "Маникюр и педикюр",
+      subcategory: {
+        title_en: "Subcategory",
+        title_ru: "Подкатегория"
+      }
     },
     price: 2500,
+    paymentType: "withoutGuarantee",
+    additionalCondition: [],
     reviews: {
       positive: 0,
       negative: 0
     },
+    executionTime: {
+      startDate: 1583676036128,
+      startTime: 1583676036128,
+      endDate: 1583676636128,
+      endTime: 1583676636128
+    },
     sdr: true,
-    business: false
+    business: false,
+    createDate: 1583676036128
   },
   {
     id: 2,
@@ -21,13 +34,26 @@ module.exports = [
       title_en: "Category",
       title_ru: "Парикмахер на дом"
     },
+    subcategory: {
+      title_en: "Subcategory",
+      title_ru: "Подкатегория"
+    },
     price: 7000,
+    paymentType: "withoutGuarantee",
+    additionalCondition: [],
     reviews: {
       positive: 0,
       negative: 0
     },
+    executionTime: {
+      startDate: 1583676036128,
+      startTime: 1583676036128,
+      endDate: 1583676636128,
+      endTime: 1583676636128
+    },
     sdr: true,
-    business: false
+    business: false,
+    createDate: 1583676006128
   },
   {
     id: 3,
@@ -36,13 +62,26 @@ module.exports = [
       title_en: "Category",
       title_ru: "Вынести строительный мусор"
     },
+    subcategory: {
+      title_en: "Subcategory",
+      title_ru: "Подкатегория"
+    },
     price: 7000,
+    paymentType: "withoutGuarantee",
+    additionalCondition: [],
     reviews: {
       positive: 2,
       negative: 0
     },
+    executionTime: {
+      startDate: 1583676036128,
+      startTime: 1583676036128,
+      endDate: 1583676636128,
+      endTime: 1583676636128
+    },
     sdr: true,
-    business: false
+    business: false,
+    createDate: 1580676036128
   },
   {
     id: 4,
@@ -51,13 +90,26 @@ module.exports = [
       title_en: "Category",
       title_ru: "Требуется администратор в банный комплекс"
     },
+    subcategory: {
+      title_en: "Subcategory",
+      title_ru: "Подкатегория"
+    },
     price: 30000,
+    paymentType: "withoutGuarantee",
+    additionalCondition: [],
     reviews: {
       positive: 0,
       negative: 1
     },
+    executionTime: {
+      startDate: 1583676036128,
+      startTime: 1583676036128,
+      endDate: 1583676636128,
+      endTime: 1583676636128
+    },
     sdr: false,
-    business: true
+    business: true,
+    createDate: 1503676036128
   },
   {
     id: 5,
@@ -66,12 +118,25 @@ module.exports = [
       title_en: "Category",
       title_ru: "Ремонт AverMedia"
     },
+    subcategory: {
+      title_en: "Subcategory",
+      title_ru: "Подкатегория"
+    },
     price: 30000,
+    paymentType: "withoutGuarantee",
+    additionalCondition: [],
     reviews: {
       positive: 1,
       negative: 0
     },
+    executionTime: {
+      startDate: 1583676036128,
+      startTime: 1583676036128,
+      endDate: 1583676636128,
+      endTime: 1583676636128
+    },
     sdr: true,
-    business: false
+    business: false,
+    createDate: 1583600036128
   }
 ];
--- youdo-clone/src/models/TaskPreview.model.ts
+++ youdo-clone/src/models/TaskPreview.model.ts
@@ -1,15 +1,23 @@
+import { Category } from "./Category.model";
+
 export interface TaskPreview {
   id: number;
   author: string;
-  category: {
-    title_en: string;
-    title_ru: string;
-  };
+  category: Category;
   price: number;
+  paymentType: string;
+  additionalCondition: [];
   reviews: {
     positive: number;
     negative: number;
   };
+  executionTime: {
+    startDate: number;
+    startTime: number;
+    endDate?: number;
+    endTime?: number;
+  };
   sdr: boolean;
   business: boolean;
+  createDate: number;
 }
