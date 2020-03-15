b1b0f5510d0afdc1eef9323fadff37a35c9eebe9
drmrhdt <drmrhdt@gmail.com>
2020-03-16 00:49:38 +0300

    Make accordion from CategoriesList

--- youdo-clone/src/app/categories-list/categories-list.component.ts
+++ youdo-clone/src/app/categories-list/categories-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Category } from "../../models/Category.model";
 import { CategoriesService } from "../../services/categories.service";
+import { Subcategory } from "../../models/Subcategory.model";
 
 @Component({
   selector: "app-categories-list",
@@ -9,8 +10,18 @@ import { CategoriesService } from "../../services/categories.service";
 })
 export class CategoriesListComponent implements OnInit {
   categories: Category[] = [];
+  subcategories: Subcategory[] = [];
+  selectedCategoryId: number;
 
   constructor(private categoriesService: CategoriesService) {}
+
+  selectCategory(category) {
+    if (category.id === this.selectedCategoryId) {
+      this.selectedCategoryId = null;
+      return;
+    }
+    this.selectedCategoryId = category.id;
+  }
 
   ngOnInit(): void {
     this.categories = this.categoriesService.categories;
