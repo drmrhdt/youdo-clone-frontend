82a6b7403c8f36fd59c0baae0a14a6a07663c987
drmrhdt <drmrhdt@gmail.com>
2020-04-26 17:44:09 +0300

    feat(modal) delete custom cancel, submit btns

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -13,25 +13,16 @@ import {
   styleUrls: ["./modal.component.scss"],
 })
 export class ModalComponent implements OnInit {
-  @Input() isShowSubmitBtn: boolean = true;
-  @Input() submitTitle: string = "Подтвердить";
   @Input() isShowCloseBtn: boolean = true;
-  @Input() isShowCancelBtn: boolean = false;
-  @Input() cancelTitle: string = "Отменить";
   @Input() modalTitle: string = "";
 
   @Input() bodyComponent: TemplateRef<any>;
 
   @Output() close: EventEmitter<null> = new EventEmitter<null>();
-  @Output() submit: EventEmitter<null> = new EventEmitter<null>();
 
   ngOnInit(): void {}
 
   closeDialog(): void {
     this.close.emit();
-  }
-
-  onSubmit(): void {
-    this.closeDialog();
   }
 }
