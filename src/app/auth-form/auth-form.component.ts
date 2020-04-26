82db6cfea4049514a143369e4eee28432e9c56c6
drmrhdt <drmrhdt@gmail.com>
2020-04-26 17:14:39 +0300

    feat(auth-form) build forms using FormBuilder
    - use formSignUp as model for sign up form and formSignIn as model for sign in form
    - map auth-form template with [formGroup], formGroupName, formControlName

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-auth-form",
@@ -7,8 +9,38 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class AuthFormComponent implements OnInit {
   @Input() authFormType: string = "signUp";
+  formSignUp: FormGroup;
+  formSignIn: FormGroup;
 
-  constructor() {}
+  constructor(private authService: AuthService, formBuilder: FormBuilder) {
+    this.formSignUp = formBuilder.group({
+      personalInfo: formBuilder.group({
+        firstName: ["", Validators.required],
+        lastName: ["", Validators.required],
+      }),
+      contacts: formBuilder.group({
+        email: [
+          "",
+          Validators.compose([Validators.required, Validators.email]),
+        ],
+      }),
+      password: ["", Validators.required],
+      passwordConfirm: ["", Validators.required],
+    });
+
+    this.formSignIn = formBuilder.group({
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      password: ["", Validators.required],
+    });
+  }
+
+  signUp() {
+    this.authService.signUp(this.formSignUp.value).subscribe(console.log);
+  }
+
+  signIn() {
+    this.authService.signIn(this.formSignIn.value).subscribe(console.log);
+  }
 
   ngOnInit(): void {}
 }
