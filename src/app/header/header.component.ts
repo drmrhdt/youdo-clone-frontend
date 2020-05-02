b42a6c72aa0c4a03ae364569297f5b8df79b5f67
drmrhdt <drmrhdt@gmail.com>
2020-05-02 04:34:59 +0300

    feat(header) check token when header onInit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -42,6 +42,7 @@ export class HeaderComponent implements OnInit {
     this.categoriesService.categories$.subscribe(
       (value) => (this.categories = value)
     );
+    this.isAuthenticated = this.authService.getAuthStatus();
     this.authService
       .getAuthStatusListener()
       .subscribe((isAuthenticated: boolean) => {
