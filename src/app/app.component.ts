d11201ea54e04402361bba2c5c34cc165a9db995
drmrhdt <drmrhdt@gmail.com>
2020-05-02 04:33:50 +0300

    feat(app) check token when app has just launched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { CategoriesService } from "../services/categories.service";
+import { AuthService } from "./services/auth.service";
 
 @Component({
   selector: "app-root",
@@ -10,7 +11,11 @@ export class AppComponent {
   title: string = "youdo-clone";
   isLoading: boolean = true;
 
-  constructor(private categoriesService: CategoriesService) {
+  constructor(
+    private categoriesService: CategoriesService,
+    private authService: AuthService
+  ) {
+    this.authService.autoAuthUser();
     this.categoriesService
       .getCategoriesWithSubcategories()
       .subscribe((response) => {
