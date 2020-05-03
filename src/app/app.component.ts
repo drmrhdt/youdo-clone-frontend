201d3874f1dd5a263c916d21a4bec89eb38f463f
drmrhdt <drmrhdt@gmail.com>
2020-05-03 23:06:09 +0300

    feat(app.component.ts) get user's info and next it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CategoriesService } from "../services/categories.service";
 import { AuthService } from "./services/auth.service";
+import { UserService } from "src/services/user.service";
 
 @Component({
   selector: "app-root",
@@ -13,16 +14,25 @@ export class AppComponent {
 
   constructor(
     private categoriesService: CategoriesService,
-    private authService: AuthService
-  ) {
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
     this.authService.autoAuthUser();
+    const isAuth = this.authService.getAuthStatus();
+
     this.categoriesService
       .getCategoriesWithSubcategories()
       .subscribe((response) => {
         this.categoriesService.categories$.next(response.data.categories);
         this.isLoading = false;
       });
-  }
 
-  ngOnInit(): void {}
+    if (isAuth) {
+      this.userService.getCurrentUserInfo().subscribe((user) => {
+        this.userService.currentUser$.next(user);
+      });
+    }
+  }
 }
