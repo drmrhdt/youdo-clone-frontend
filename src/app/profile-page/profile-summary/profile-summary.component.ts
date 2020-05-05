e1a2013f0eb3f8933e5c20609a9bd89f34e9a36a
drmrhdt <drmrhdt@gmail.com>
2020-05-05 22:09:02 +0300

    feat(profile-summary) fill with data

--- src/app/profile-page/profile-summary/profile-summary.component.ts
+++ src/app/profile-page/profile-summary/profile-summary.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { IUser } from "src/models/IUser.model";
 
 @Component({
-  selector: 'app-profile-summary',
-  templateUrl: './profile-summary.component.html',
-  styleUrls: ['./profile-summary.component.scss']
+  selector: "app-profile-summary",
+  templateUrl: "./profile-summary.component.html",
+  styleUrls: ["./profile-summary.component.scss"],
 })
 export class ProfileSummaryComponent implements OnInit {
+  @Input() user: IUser;
+  @Input() isMyProfile: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get userIsOnline() {
+    return window.navigator.onLine;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
