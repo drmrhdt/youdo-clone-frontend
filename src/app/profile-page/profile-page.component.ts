bf7d5629e4e73e7719280524a548b21ed42e1b68
drmrhdt <drmrhdt@gmail.com>
2020-05-05 16:24:14 +0300

    feat(profile-page.component.ts) figure out whose profile
    - get id from url and currentUser$ and compare it with my id to figure out is it my profile or another user
    
    fix(profile-page.component.ts) !this.isMyProfile

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/services/user.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-profile-page',
-  templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  selector: "app-profile-page",
+  templateUrl: "./profile-page.component.html",
+  styleUrls: ["./profile-page.component.scss"],
 })
 export class ProfilePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get idFromUrl(): string {
+    return this.route.snapshot.paramMap.get("id");
   }
 
+  get isMyProfile(): boolean {
+    return this.idFromUrl === this.signedInUserId;
+  }
+
+  signedInUserId: string = "";
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.currentUser$.subscribe(
+      (response) => (this.signedInUserId = response.data.currentUser._id)
+    );
+
+    if (!this.isMyProfile) {
+      this.userService.getUserInfoById(this.idFromUrl).subscribe(console.log);
+    }
+  }
 }
