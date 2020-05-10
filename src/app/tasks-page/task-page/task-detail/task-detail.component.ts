8c0216abc58ca846f9ae896bf4e7581a0c51f706
drmrhdt <drmrhdt@gmail.com>
2020-05-10 17:42:36 +0300

    feat(task-detail) subscribe signed in user, add onSubmit
    - conditionally console.log test text

--- src/app/tasks-page/task-page/task-detail/task-detail.component.ts
+++ src/app/tasks-page/task-page/task-detail/task-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { TaskService } from "../../../../services/task.service";
 import { ActivatedRoute } from "@angular/router";
+import { TaskService } from "../../../../services/task.service";
+import { UserService } from "src/services/user.service";
 import { ITask } from "../../../../models/ITask.model";
+import { ITaskResponse } from "src/models/ITaskResponse.model";
+import { IUser } from "src/models/IUser.model";
 
 @Component({
   selector: "app-task-detail",
@@ -9,20 +12,52 @@ import { ITask } from "../../../../models/ITask.model";
   styleUrls: ["./task-detail.component.scss"],
 })
 export class TaskDetailComponent implements OnInit {
-  constructor(
-    private taskService: TaskService,
-    private route: ActivatedRoute
-  ) {}
-
-  isLoading: boolean;
+  isLoading: boolean = true;
   task: ITask;
+  signedInUserId: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
     const _id = this.route.snapshot.paramMap.get("taskId");
-    this.taskService.getTaskById(_id).subscribe((response) => {
+
+    // TODO execute services sequantually
+    // concat(
+    //   this.taskService.getTaskById(_id),
+    //   this.userService.currentUserListener$
+    // ).subscribe((response) => {
+    //   console.log(response);
+    //   this.task = response.data.task;
+    //   console.log(response._id);
+    //   this.signedInUserId = response._id;
+    //   // console.log(this.task);
+    //   // console.log(this.signedInUserId);
+    //   this.isLoading = false;
+    // });
+
+    // this.taskService.getTaskById(_id).pipe(flatMap(()=>this.userService.currentUserListener$))
+
+    this.taskService.getTaskById(_id).subscribe((response: ITaskResponse) => {
       this.isLoading = false;
-      return (this.task = response.data.task);
+      this.task = response.data.task;
     });
+
+    this.userService.currentUserListener$.subscribe((response: IUser) => {
+      this.signedInUserId = response._id;
+    });
+  }
+
+  onSubmit(): void {
+    // TODO it works only after we go to another route and return here
+    if (this.signedInUserId) {
+      console.log("ok, but are you executor?");
+    } else {
+      console.log("no, you must sign in");
+    }
   }
 }
