6c4677d541b82d5ef5628e97b62b0379b085ee3b
drmrhdt <drmrhdt@gmail.com>
2020-05-17 01:59:27 +0300

    feat(verification-banner.*): add link to verification-form

--- src/app/shared/verification-banner/verification-banner.component.ts
+++ src/app/shared/verification-banner/verification-banner.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-verification-banner',
-  templateUrl: './verification-banner.component.html',
-  styleUrls: ['./verification-banner.component.scss']
+  selector: "app-verification-banner",
+  templateUrl: "./verification-banner.component.html",
+  styleUrls: ["./verification-banner.component.scss"],
 })
 export class VerificationBannerComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.router.navigateByUrl("youdo-clone/verification/personalinfo");
   }
-
 }
