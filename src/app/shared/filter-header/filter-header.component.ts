a4dac0aeedff79cb098159f500ad31ff0c84e772
drmrhdt <drmrhdt@gmail.com>
2020-05-17 21:04:36 +0300

    feat(filter-header.component): decorate title and content with @Input
    - TODO need delete some old content in template

--- src/app/shared/filter-header/filter-header.component.ts
+++ src/app/shared/filter-header/filter-header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from "@angular/core";
 
 @Component({
-  selector: 'app-filter-header',
-  templateUrl: './filter-header.component.html',
-  styleUrls: ['./filter-header.component.scss']
+  selector: "app-filter-header",
+  templateUrl: "./filter-header.component.html",
+  styleUrls: ["./filter-header.component.scss"],
 })
 export class FilterHeaderComponent implements OnInit {
+  @Input() title: string = "Все категории";
+  @Input() bodyComponent: TemplateRef<any>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
