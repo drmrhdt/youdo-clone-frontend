1e18ea27ffe9df2dc4202c894cb2993854773433
drmrhdt <drmrhdt@gmail.com>
2020-05-17 21:08:11 +0300

    feat(tasks-page.component.*): add loading, push content to app-filter-header
    - show or not content depends on isLoading
    - push content to app-filter-header depends on url

--- src/app/tasks-page/tasks-page.component.ts
+++ src/app/tasks-page/tasks-page.component.ts
@@ -12,30 +12,39 @@ import { IUser } from "src/models/IUser.model";
   styleUrls: ["./tasks-page.component.scss"],
 })
 export class TasksPageComponent implements OnInit {
+  isLoading: boolean = true;
+  tasks: ITask[] = [];
+  signedInUserId: string;
+  title: string = this.isMyTasks ? "Мои заказы" : "Все категории";
+
+  get isMyTasks() {
+    return this.route.snapshot.url[2].path === "my";
+  }
+
   constructor(
     private taskService: TaskService,
     private route: ActivatedRoute,
     private userService: UserService
   ) {}
 
-  tasks: ITask[] = [];
-  signedInUserId: string;
-
   ngOnInit(): void {
     this.userService.currentUserListener$.subscribe((response: IUser) => {
       if (response) this.signedInUserId = response._id;
+      if (this.isMyTasks) {
+        this.taskService
+          .getTasksListByFilter("authorId", this.signedInUserId)
+          .subscribe((response: ITasksResponse) => {
+            this.tasks = response.data.tasks;
+            this.isLoading = false;
+          });
+      } else {
+        this.taskService
+          .getTasksList()
+          .subscribe((response: ITasksResponse) => {
+            this.tasks = response.data.tasks;
+            this.isLoading = false;
+          });
+      }
     });
-
-    if (this.route.snapshot.url[2].path === "my") {
-      this.taskService
-        .getTasksListByFilter("authorId", this.signedInUserId)
-        .subscribe((response: ITasksResponse) => {
-          this.tasks = response.data.tasks;
-        });
-    } else {
-      this.taskService.getTasksList().subscribe((response: ITasksResponse) => {
-        this.tasks = response.data.tasks;
-      });
-    }
   }
 }
