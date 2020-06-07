dbdf57d02be33d38fc4662c8bc74fbd0625b480a
drmrhdt <drmrhdt@gmail.com>
2020-06-07 22:39:32 +0300

    fix(executors-page.component.*): temp fix for tab

--- src/app/executors-page/executors-page.component.ts
+++ src/app/executors-page/executors-page.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { UserService } from "src/services/user.service";
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import { IUser } from "../../models/IUser.model";
-import { IUsersResponse } from "src/models/IUsersResponse.model";
-import { Filters } from "src/models/enum/Filters";
+import { Component, OnInit, OnDestroy } from "@angular/core"
+import { UserService } from "src/services/user.service"
+import { Subject } from "rxjs"
+import { takeUntil } from "rxjs/operators"
+import { IUser } from "../../models/IUser.model"
+import { IUsersResponse } from "src/models/IUsersResponse.model"
+import { Filters } from "src/models/enum/Filters"
 
 @Component({
   selector: "app-executors-page",
@@ -12,11 +12,11 @@ import { Filters } from "src/models/enum/Filters";
   styleUrls: ["./executors-page.component.scss"],
 })
 export class ExecutorsPageComponent implements OnInit, OnDestroy {
-  users: IUser[] = [];
-  filters = Filters;
-  tab: Filters;
+  users: IUser[] = []
+  filters = Filters
+  tab: string
 
-  private _unsubscriber$ = new Subject();
+  private _unsubscriber$ = new Subject()
 
   constructor(private userService: UserService) {}
 
@@ -26,15 +26,15 @@ export class ExecutorsPageComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this._unsubscriber$))
       .subscribe(
         (response: IUsersResponse) => (this.users = response.data.users)
-      );
+      )
   }
 
   ngOnDestroy(): void {
-    this._unsubscriber$.next(true);
-    this._unsubscriber$.complete();
+    this._unsubscriber$.next(true)
+    this._unsubscriber$.complete()
   }
 
-  onTabClick(filter: Filters): void {
-    this.tab = filter;
+  onTabClick(filter: string): void {
+    this.tab = filter
   }
 }
