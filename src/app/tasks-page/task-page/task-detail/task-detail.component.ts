2335a0295b98e0f5a0bd51276ede713249b47b4a
drmrhdt <drmrhdt@gmail.com>
2020-06-07 20:01:43 +0300

    refactor(task-detail.component.ts): fetch data sequantially, delete reduntant properties

--- src/app/tasks-page/task-page/task-detail/task-detail.component.ts
+++ src/app/tasks-page/task-page/task-detail/task-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import { FormBuilder, Validators } from "@angular/forms";
+import { takeUntil, flatMap } from "rxjs/operators";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { TaskService } from "../../../../services/task.service";
 import { UserService } from "src/services/user.service";
 import { ITask } from "../../../../models/ITask.model";
@@ -23,9 +23,9 @@ export class TaskDetailComponent implements OnInit, OnDestroy {
   isLoading: boolean = true;
   isShowDialog: boolean = false;
   task: ITask;
-  signedInUserId: string;
   signedInUser: IUser;
   suggestion: IPossibleExecutorSuggestion;
+  form: FormGroup;
 
   private _unsubscriber$ = new Subject();
 
@@ -35,57 +35,36 @@ export class TaskDetailComponent implements OnInit, OnDestroy {
     private taskService: TaskService,
     private userService: UserService,
     private suggestionService: SuggestionService
-  ) {}
-
-  form = this.formBuilder.group({
-    paymentType: ["cash", Validators.required],
-    price: [200, [Validators.required, Validators.min(200)]],
-    commentary: "",
-  });
+  ) {
+    this.form = this.formBuilder.group({
+      paymentType: ["cash", Validators.required],
+      price: [200, [Validators.required, Validators.min(200)]],
+      commentary: "",
+    });
+  }
 
   ngOnInit(): void {
-    this.isLoading = true;
-    const _id = this.route.snapshot.paramMap.get("taskId");
-
-    // TODO execute services sequantually
-    // concat(
-    //   this.taskService.getTaskById(_id),
-    //   this.userService.currentUserListener$
-    // ).subscribe((response) => {
-    //   console.log(response);
-    //   this.task = response.data.task;
-    //   console.log(response._id);
-    //   this.signedInUserId = response._id;
-    //   // console.log(this.task);
-    //   // console.log(this.signedInUserId);
-    //   this.isLoading = false;
-    // });
-
-    // this.taskService.getTaskById(_id).pipe(flatMap(()=>this.userService.currentUserListener$))
+    const _id = this.route.snapshot.params.taskId;
 
     this.taskService
       .getTaskById(_id)
-      .pipe(takeUntil(this._unsubscriber$))
-      .subscribe((response: ITaskResponse) => {
-        this.task = response.data.task;
-
-        this.userService.currentUserListener$
-          .pipe(takeUntil(this._unsubscriber$))
-          .subscribe((response: IUser) => {
-            this.signedInUserId = response._id;
-            this.signedInUser = response;
-
-            this.suggestionService
-              .getSuggestionByTaskIdAndExecutorId(
-                this.task._id,
-                this.signedInUserId
-              )
-              .pipe(takeUntil(this._unsubscriber$))
-              .subscribe((response: ISuggestionResponse) => {
-                this.suggestion = response.data.suggestion;
-                this.isLoading = false;
-              });
-          });
+      .pipe(
+        takeUntil(this._unsubscriber$),
+        flatMap((response: ITaskResponse) => {
+          this.task = response.data.task;
+          return this.userService.currentUserListener$;
+        }),
+        flatMap((response: IUser) => {
+          this.signedInUser = response;
+          return this.suggestionService.getSuggestionByTaskIdAndExecutorId(
+            this.task._id,
+            this.signedInUser._id
+          );
+        })
+      )
+      .subscribe((response: ISuggestionResponse) => {
+        this.suggestion = response.data.suggestion;
+        this.isLoading = false;
       });
   }
 
@@ -101,14 +80,14 @@ export class TaskDetailComponent implements OnInit, OnDestroy {
 
   onSubmit(): void {
     if (
-      !this.form.invalid &&
-      this.signedInUserId &&
+      this.form.valid &&
+      this.signedInUser &&
       this.signedInUser.workInfo.isExecutor
     ) {
       this.suggestionService
         .addNewSuggestion({
           taskId: this.task._id,
-          executorId: this.signedInUserId,
+          executorId: this.signedInUser._id,
           ...this.form.value,
         })
         .pipe(takeUntil(this._unsubscriber$))
