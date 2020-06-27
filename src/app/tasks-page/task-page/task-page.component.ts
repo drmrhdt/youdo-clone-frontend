dfea7ce32113373745847b2f5f76aa783dc5f434
drmrhdt <drmrhdt@gmail.com>
2020-06-27 23:53:23 +0300

    fix(task-page.component.ts): fetch if _id is there

--- src/app/tasks-page/task-page/task-page.component.ts
+++ src/app/tasks-page/task-page/task-page.component.ts
@@ -39,30 +39,31 @@ export class TaskPageComponent implements OnInit, OnDestroy {
 	ngOnInit(): void {
 		const _id = this._route.snapshot.params.taskId
 
-		this._taskService
-			.getTaskById(_id)
-			.pipe(
-				takeUntil(this._unsubscriber$),
-				flatMap((response: ITaskResponse) => {
-					this.task = response.data.task
-					return this._userService.currentUserListener$
-				}),
-				flatMap((response: IUser) => {
-					this.signedInUser = response
-					return this._suggestionService.getSuggestionByTaskIdAndExecutorId(
-						this.task._id,
-						this.signedInUser._id
-					)
-				}),
-				flatMap((response: ISuggestionResponse) => {
-					this.suggestion = response.data.suggestion
-					return this._userService.getUserInfoById(this.task.authorId)
+		if (_id)
+			this._taskService
+				.getTaskById(_id)
+				.pipe(
+					takeUntil(this._unsubscriber$),
+					flatMap((response: ITaskResponse) => {
+						this.task = response.data.task
+						return this._userService.currentUserListener$
+					}),
+					flatMap((response: IUser) => {
+						this.signedInUser = response
+						return this._suggestionService.getSuggestionByTaskIdAndExecutorId(
+							this.task._id,
+							this.signedInUser._id
+						)
+					}),
+					flatMap((response: ISuggestionResponse) => {
+						this.suggestion = response.data.suggestion
+						return this._userService.getUserInfoById(this.task.authorId)
+					})
+				)
+				.subscribe((response: IUserResponse) => {
+					this.taskAuthor = response.data.findedByIdUser
+					this.isLoading = false
 				})
-			)
-			.subscribe((response: IUserResponse) => {
-				this.taskAuthor = response.data.findedByIdUser
-				this.isLoading = false
-			})
 	}
 
 	ngOnDestroy(): void {
