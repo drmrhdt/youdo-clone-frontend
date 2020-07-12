1dfadafa37d3bfeedf6aa70ab8a5a5143db7f632
drmrhdt <drmrhdt@gmail.com>
2020-07-12 20:42:37 +0300

    feat: delete 'youdo-clone' from routerLink

--- src/app/executors-page/executor-preview/executor-preview.component.ts
+++ src/app/executors-page/executor-preview/executor-preview.component.ts
@@ -4,19 +4,19 @@ import { Router } from '@angular/router'
 import { IUser } from 'src/models'
 
 @Component({
-	selector: 'app-executor-preview',
-	templateUrl: './executor-preview.component.html',
-	styleUrls: ['./executor-preview.component.scss']
+    selector: 'app-executor-preview',
+    templateUrl: './executor-preview.component.html',
+    styleUrls: ['./executor-preview.component.scss']
 })
 export class ExecutorPreviewComponent implements OnInit {
-	@Input() executor: IUser
+    @Input() executor: IUser
 
-	constructor(private _router: Router) {}
+    constructor(private _router: Router) {}
 
-	// TODO rewrite with routerLink
-	navigateToUserProfile(id: string): void {
-		this._router.navigateByUrl(`/youdo-clone/profile/${id}`)
-	}
+    // TODO rewrite with routerLink
+    navigateToUserProfile(id: string): void {
+        this._router.navigateByUrl(`/profile/${id}`)
+    }
 
-	ngOnInit(): void {}
+    ngOnInit(): void {}
 }
