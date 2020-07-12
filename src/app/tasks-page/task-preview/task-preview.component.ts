28fcb77791b59c3bfe994803b60c9e45575ce037
drmrhdt <drmrhdt@gmail.com>
2020-07-12 20:50:48 +0300

    refactor(task-preview.component.*): write (click) instead of routerLink

--- src/app/tasks-page/task-preview/task-preview.component.ts
+++ src/app/tasks-page/task-preview/task-preview.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core'
+import { Router } from '@angular/router'
 
 import { ITask } from 'src/models'
 
 @Component({
-	selector: 'app-task-preview',
-	templateUrl: './task-preview.component.html',
-	styleUrls: ['./task-preview.component.scss']
+    selector: 'app-task-preview',
+    templateUrl: './task-preview.component.html',
+    styleUrls: ['./task-preview.component.scss']
 })
 export class TaskPreviewComponent {
-	@Input() task: ITask
+    @Input() task: ITask
 
-	constructor() {}
+    constructor(private _router: Router) {}
+
+    selectTask(id: string): void {
+        this._router.navigateByUrl(`/tasks/${id}`)
+    }
 }
