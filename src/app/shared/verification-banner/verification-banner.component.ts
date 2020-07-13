3f3ef9290b390ad310f2225bb413b386b406dd4e
drmrhdt <drmrhdt@gmail.com>
2020-07-13 03:14:45 +0300

    feat(verification-banner.component.ts): delete 'youdo-clone' from navigation

--- src/app/shared/verification-banner/verification-banner.component.ts
+++ src/app/shared/verification-banner/verification-banner.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core"
-import { Router } from "@angular/router"
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
-  selector: "app-verification-banner",
-  templateUrl: "./verification-banner.component.html",
-  styleUrls: ["./verification-banner.component.scss"],
+    selector: 'app-verification-banner',
+    templateUrl: './verification-banner.component.html',
+    styleUrls: ['./verification-banner.component.scss']
 })
 export class VerificationBannerComponent {
-  constructor(private _router: Router) {}
+    constructor(private _router: Router) {}
 
-  onClick(): void {
-    this._router.navigateByUrl("youdo-clone/verification/personalinfo")
-  }
+    onClick(): void {
+        this._router.navigateByUrl('verification/personalinfo')
+    }
 }
