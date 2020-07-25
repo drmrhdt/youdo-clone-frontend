5231986ea33f4bec9e32fdf385a6c0f2918c227e
drmrhdt <drmrhdt@gmail.com>
2020-07-25 17:22:16 +0300

    feat(task-page.component.ts): delete suggestionsService

--- src/app/tasks/task-page/task-page.component.ts
+++ src/app/tasks/task-page/task-page.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 
-import { Subject, of } from 'rxjs'
+import { Subject } from 'rxjs'
 import { takeUntil, flatMap } from 'rxjs/operators'
 
-import { UserService, TaskService, SuggestionService } from 'src/services'
+import { UserService, TaskService } from 'src/services'
 
-import {
-    ITask,
-    IUser,
-    IPossibleExecutorSuggestion,
-    ITaskResponse,
-    IUserResponse,
-    ISuggestionResponse
-} from 'src/models'
+import { ITask, IUser, ITaskResponse } from 'src/models'
 
 @Component({
     selector: 'app-task-page',
@@ -23,58 +16,41 @@ import {
 export class TaskPageComponent implements OnInit, OnDestroy {
     isLoading: boolean = true
     task: ITask
-    taskAuthor: IUser
     signedInUser: IUser
-    suggestion: IPossibleExecutorSuggestion
 
     private _unsubscriber$ = new Subject()
 
     constructor(
         private _route: ActivatedRoute,
         private _taskService: TaskService,
-        private _userService: UserService,
-        private _suggestionService: SuggestionService
+        private _userService: UserService
     ) {}
 
     ngOnInit(): void {
         const _id = this._route.snapshot.params.taskId
 
-        if (_id) {
-            this._taskService
-                .getTaskById(_id)
-                .pipe(
-                    takeUntil(this._unsubscriber$),
-                    flatMap((response: ITaskResponse) => {
+        // TODO temp fix
+        setTimeout(() => {
+            if (_id) {
+                this._userService.currentUserListener$
+                    .pipe(
+                        takeUntil(this._unsubscriber$),
+                        flatMap((response: IUser) => {
+                            if (response) {
+                                this.signedInUser = response
+                                return this._taskService.getTaskById(
+                                    _id,
+                                    this.signedInUser._id
+                                )
+                            }
+                        })
+                    )
+                    .subscribe((response: ITaskResponse) => {
                         this.task = response.data.task
-                        return this._userService.getUserInfoById(
-                            this.task.authorId
-                        )
+                        this.isLoading = false
                     })
-                )
-                .subscribe((response: IUserResponse) => {
-                    this.isLoading = false
-                    this.taskAuthor = response.data.findedByIdUser
-                })
-
-            this._userService.currentUserListener$
-                .pipe(
-                    takeUntil(this._unsubscriber$),
-                    flatMap(response => {
-                        this.signedInUser = response
-                        if (response) {
-                            return this._suggestionService.getSuggestionByTaskIdAndExecutorId(
-                                _id,
-                                this.signedInUser._id
-                            )
-                        } else return of(null)
-                    })
-                )
-                .subscribe((response: ISuggestionResponse) => {
-                    response
-                        ? (this.suggestion = response.data.suggestion)
-                        : (this.suggestion = null)
-                })
-        }
+            }
+        }, 500)
     }
 
     ngOnDestroy(): void {
