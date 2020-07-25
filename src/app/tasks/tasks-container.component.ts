25ae76c665773a27beb5c44823587c46af28e882
drmrhdt <drmrhdt@gmail.com>
2020-07-25 17:49:44 +0300

    refactor(tasks-container): delete scss, html, spec.ts

--- src/app/tasks/tasks-container.component.ts
+++ src/app/tasks/tasks-container.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-tasks-container',
-  templateUrl: './tasks-container.component.html',
-  styleUrls: ['./tasks-container.component.scss']
+    selector: 'app-tasks-container',
+    template: '<router-outlet></router-outlet>'
 })
-export class TasksContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TasksContainerComponent {}
