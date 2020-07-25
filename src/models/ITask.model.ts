9e8bdc199a1b67f3fd09b6be40ef4dbcaa21af8d
drmrhdt <drmrhdt@gmail.com>
2020-07-25 17:21:43 +0300

    feat(task): fix suggestions everywhere

--- src/models/ITask.model.ts
+++ src/models/ITask.model.ts
@@ -1,34 +1,34 @@
-import { IPossibleExecutorSuggestion } from "./IPossibleExecutorSuggestion.model";
+import { IPossibleExecutorSuggestion } from './IPossibleExecutorSuggestion.model'
 
 export interface ITask {
-  _id: any;
-  description: string;
-  category: string; // do I need it?
-  subcategory: string;
-  comment: string;
-  time: string;
-  executionTime: {
-    startDate: null | number | string;
-    startTime: null | number | string;
-    endDate?: null | number | string;
-    endTime?: null | number | string;
-  };
-  address: string; // it will be FormArray
-  budget: number;
-  author: string;
-  authorId: string;
-  email: string;
-  tel: string;
-  isBusiness: false;
-  additionalConditions: {
-    isSubscribeSuggestions: false;
-    isShowOnlyToExecutors: false;
-  };
-  reviews: {
-    positive: number;
-    negative: number;
-  };
-  isSbr: boolean;
-  createDate: number;
-  possibleExecutorSuggestions: IPossibleExecutorSuggestion[];
+    _id: any
+    description: string
+    category: string // do I need it?
+    subcategory: string
+    comment: string
+    time: string
+    executionTime: {
+        startDate: null | number | string
+        startTime: null | number | string
+        endDate?: null | number | string
+        endTime?: null | number | string
+    }
+    address: string // it will be FormArray
+    budget: number
+    author: string
+    authorId: string
+    email: string
+    tel: string
+    isBusiness: false
+    additionalConditions: {
+        isSubscribeSuggestions: false
+        isShowOnlyToExecutors: false
+    }
+    reviews: {
+        positive: number
+        negative: number
+    }
+    isSbr: boolean
+    createDate: number
+    suggestions: IPossibleExecutorSuggestion[]
 }
