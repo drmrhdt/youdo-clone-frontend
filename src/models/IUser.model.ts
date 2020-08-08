f4fac52b7f11793b308b313a08bbb41660327dd3
drmrhdt <drmrhdt@gmail.com>
2020-08-08 22:13:06 +0300

    fix(IUser.model.ts): delete id from avatar

--- src/models/IUser.model.ts
+++ src/models/IUser.model.ts
@@ -1,59 +1,58 @@
 export interface IUser {
-  personalInfo: {
-    firstName: string;
-    middleName: string;
-    lastName: string;
-    username: string;
-    gender: string;
-    birthday: string;
-    age: number;
-    description: string;
-    avatar: {
-      id: string;
-    };
-    city: {
-      id: string;
-      name: string;
-    };
-    address: string;
-  };
-  contacts: {
-    phone: string;
-    email: string;
-  };
-  workInfo: {
-    isMaster: boolean;
-    isExecutor: boolean;
-    isAvailable: boolean;
-    categories: number;
-    subcategories: number;
-    serviceExamples: string;
-    сompanyName: string;
-    reviews: {
-      positive: number;
-      negative: number;
-      total: number;
-    };
-  };
-  taskInfo: {
-    total: number;
-    inProcess: number;
-    finishedTasks: number;
-    successfulTasks: number;
-    failedTasks: number;
-  };
-  moderationInfo: {
-    isBanned: boolean;
-    role: string;
-  };
-  lastActivityDate: string;
-  viewsCounter: number;
-  isOnline: boolean;
-  password: string;
-  passwordConfirm: string;
-  passwordChangedAt: Date;
-  passwordResetToken: string;
-  passwordResetExpires: Date;
-  isActiveAccount: boolean;
-  _id: string;
+    personalInfo: {
+        firstName: string
+        middleName: string
+        lastName: string
+        username: string
+        gender: string
+        birthday: string
+        age: number
+        description: string
+        avatar: string
+
+        city: {
+            id: string
+            name: string
+        }
+        address: string
+    }
+    contacts: {
+        phone: string
+        email: string
+    }
+    workInfo: {
+        isMaster: boolean
+        isExecutor: boolean
+        isAvailable: boolean
+        categories: number
+        subcategories: number
+        serviceExamples: string
+        сompanyName: string
+        reviews: {
+            positive: number
+            negative: number
+            total: number
+        }
+    }
+    taskInfo: {
+        total: number
+        inProcess: number
+        finishedTasks: number
+        successfulTasks: number
+        failedTasks: number
+    }
+    moderationInfo: {
+        isBanned: boolean
+        role: string
+    }
+    lastActivityDate: string
+    viewsCounter: number
+    isOnline: boolean
+    password: string
+    passwordConfirm: string
+    passwordChangedAt: Date
+    passwordResetToken: string
+    passwordResetExpires: Date
+    isActiveAccount: boolean
+    _id: string
 }
