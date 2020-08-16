1c820a7bb5e39044d15b670183600afcdc42c846
drmrhdt <drmrhdt@gmail.com>
2020-08-16 12:01:11 +0300

    feat: update taskInfo's total property when choose/cancel executor

--- src/app/tasks/suggestion/suggestion.component.ts
+++ src/app/tasks/suggestion/suggestion.component.ts
@@ -7,12 +7,17 @@ import {
 } from '@angular/core'
 import { Router } from '@angular/router'
 
-import { Subject } from 'rxjs'
+import { Subject, Observable } from 'rxjs'
 import { takeUntil, flatMap } from 'rxjs/operators'
 
 import { TaskService, UserService, SuggestionService } from 'src/services'
 
-import { IPossibleExecutorSuggestion, IUser } from 'src/models'
+import {
+    IPossibleExecutorSuggestion,
+    IUser,
+    IUserResponse,
+    ITaskResponse
+} from 'src/models'
 
 @Component({
     selector: 'app-suggestion',
@@ -21,12 +26,13 @@ import { IPossibleExecutorSuggestion, IUser } from 'src/models'
 })
 export class SuggestionComponent implements OnDestroy {
     @Input() suggestion: IPossibleExecutorSuggestion
-    @Input() chosedExecutor: string
+    @Input() chosedExecutor: IUser
     @Input() isMyTask: boolean
 
     @Output() onEdit = new EventEmitter()
 
     signedInUser: IUser
+    isShowModal: boolean = false
 
     private _unsubscriber$ = new Subject()
 
@@ -52,17 +58,51 @@ export class SuggestionComponent implements OnDestroy {
             .updateTask(this.suggestion.taskId, {
                 executor: executorId
             })
-            .pipe(takeUntil(this._unsubscriber$))
-            .subscribe(response => {
-                if (response.data.updatedTask.executor._id === executorId)
-                    this.chosedExecutor = executorId
-            })
+            .pipe(
+                takeUntil(this._unsubscriber$),
+                flatMap(response => {
+                    if (response.data.updatedTask.executor._id === executorId) {
+                        this.chosedExecutor = response.data.updatedTask.executor
+
+                        return this.updateChosedExecutorTaskInfoTotal(
+                            response.data.updatedTask.executor,
+                            'inc'
+                        )
+                    }
+                })
+            )
+            .subscribe(() => this.toggleModal())
     }
 
     onCancelClick(): void {
         this.deleteExecutorFromTask()
-            .pipe(takeUntil(this._unsubscriber$))
+            .pipe(
+                takeUntil(this._unsubscriber$),
+                flatMap(() =>
+                    this.updateChosedExecutorTaskInfoTotal(
+                        this.chosedExecutor,
+                        'dec'
+                    )
+                )
+            )
             .subscribe(() => (this.chosedExecutor = null))
+    }
+
+    updateChosedExecutorTaskInfoTotal(
+        executor,
+        action: 'inc' | 'dec'
+    ): Observable<IUserResponse> {
+        const taskInfo = {
+            ...executor.taskInfo,
+            total:
+                action === 'inc'
+                    ? ++executor.taskInfo.total
+                    : --executor.taskInfo.total
+        }
+
+        return this._userService.updateUser(executor._id, {
+            taskInfo
+        })
     }
 
     onEditClick(suggestion): void {
@@ -79,7 +119,7 @@ export class SuggestionComponent implements OnDestroy {
             .subscribe(() => (this.chosedExecutor = null))
     }
 
-    deleteExecutorFromTask() {
+    deleteExecutorFromTask(): Observable<ITaskResponse> {
         return this._taskService.updateTask(this.suggestion.taskId, {
             executor: null
         })
@@ -87,6 +127,10 @@ export class SuggestionComponent implements OnDestroy {
 
     navigateToUserProfile(id: string): void {
         this._router.navigate(['profile', id])
+    }
+
+    toggleModal(): void {
+        this.isShowModal = !this.isShowModal
     }
 
     ngOnDestroy(): void {
